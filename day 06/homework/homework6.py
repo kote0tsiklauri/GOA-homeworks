e75abbc2b827643f91510b6bd514d67399e1fbd7
kote0tsiklauri <kotetsiklaluri@gmail.com>
2024-06-02 14:00:27 +0400

    commit

---
+++ day 06/homework/homework6.py
@@ -0,0 +1,18 @@
+first_name = (input("name: "))
+
+print("congrants",first_name)
+
+age1 = int(input("mothers age: "))
+age2 = int(input("fathers age: "))
+
+print("in 20 years your mother will be",age1 +20,"and your father will be",age2 +20,)
+
+name = input("first_name: ")
+surname = input("last_name: ")
+age = input("age: ")
+gmail = input("gmail: ")
+address = input("address: ")
+
+print("your name is:",name + ".last name:",surname + ".you are",age,"years old." + "gmail address:",gmail + ".address: ",address)
+
+
