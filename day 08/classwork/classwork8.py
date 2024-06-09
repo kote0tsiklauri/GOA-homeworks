a11b7666e1b29dca1a76d8bb7688a7af07f903d4
kote0tsiklauri <kotetsiklaluri@gmail.com>
2024-06-09 14:59:23 +0400

    commit

---
+++ day 08/classwork/classwork8.py
@@ -0,0 +1,13 @@
+num1 = int(input("whats your age: "))
+
+print(num1 >= 25 and num1 >=18)
+
+
+
+
+num2 = int(input("whats your buget: "))
+
+print(num2 <=100 and num2 >= 50)
+
+for i in range(10):
+    print("kote")
---
+++ day 08/homework/homework8.py
@@ -0,0 +1,20 @@
+# num1 = int(input("first number: "))
+# num2 = int(input("second number: "))
+# num3 = int(input("thიrd number: "))
+# num4 = int(input("fourth number: "))
+# num5 = int(input("fifth number: "))
+
+# print(num1 + num2 + num3 + num4 + num5)
+# print(num1 / num2 / num3 / num4 / num5)
+# print(num1 * num2 * num3 * num4 * num5)
+# print(num1 - num2 - num3 - num4 - num5)
+
+# age = int(input("your age: "))
+ 
+# print(age > 18 and age <20)
+
+num1 = input("inpute whole number: ")
+num2 = input("inpute decimal: ")
+
+print(type(num1))
+print(type(num2))
