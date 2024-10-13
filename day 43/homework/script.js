3442543d714ab44e3a16845636bc0b99c4e6f54f
kote0tsiklauri <kotetsiklaluri@gmail.com>
2024-10-13 14:50:41 +0400

    day 43

---
+++ day 43/homework/script.js
@@ -0,0 +1,20 @@
+const p1 = document.getElementById('p1')
+const p2 = document.getElementById('p2')
+const p3 = document.getElementById('p3')
+const p4 = document.getElementById('p4')
+const p5 = document.getElementById('p5')
+const p6 = document.getElementById('p6')
+
+p1.textContent = "hi"
+
+p2.style.color = "blue"
+
+p3.textContent = "hi"
+p3.style.backgroundColor = "blue"
+
+p4.style.fontSize = "30px"
+p4.style.textAlign = "center"
+p4.textContent = "hi"
+
+p5.style.color = "red"
+p6.style.color = "blue"
